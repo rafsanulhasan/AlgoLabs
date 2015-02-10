4622e7d313b6ec5561423b5e354fc8808ffeb301
rafsan <rafsan_14@hotmail.com>
2015-02-11 00:58:15 +0600

    Added SortOrders .cs file and enum

--- AlgoLabs/ExtensionMethods.cs
+++ AlgoLabs/ExtensionMethods.cs
@@ -8,5 +8,23 @@ namespace AlgoLabs
 {
     public static class ExtensionMethods
     {
+         public static int[] Sort(this int[] array, SortAlgorithms algorithm = SortAlgorithms.Automatic)
+         {
+              int[] result = new int[array.Length];
+              switch (algorithm)             
+              {
+                   case SortAlgorithms.Heap:
+                        break;
+                   case SortAlgorithms.Insertion:
+                        break;
+                   case SortAlgorithms.Merge: 
+                        break;
+                   case SortAlgorithms.Quick:
+                        break;
+                   default:
+                        break;
+              }
+              return result;
+         }
     }
 }
