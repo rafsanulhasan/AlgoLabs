f0c313f705e939a206c837ecbe1afcb66acd0d6c
rafsan <rafsan_14@hotmail.com>
2015-02-22 09:10:32 +0600

    Added PositionInfo struct

---
+++ AlgoLabs/PositionInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+     public struct PositionInfo
+     {
+          public int Left { get { return Int32.Parse(Range.Split('-')[0]); } set { Range.Split('-')[0] = value.ToString(); } }
+          public int Right { get { return Int32.Parse(Range.Split('-')[1]); } set { Range.Split('-')[1] = value.ToString(); } }
+          private string Range;
+          public PositionInfo(int[] range)
+          {
+               if (range.Length != 2)
+                    throw new Exception("Conversion failed");
+               Range = String.Format("{0}-{1}", range[0], range[1]);
+          }
+          public PositionInfo(int left, int right)
+          {
+               Range = String.Format("{0}-{1}", left, right);
+          }
+          public PositionInfo(string range)
+          {
+               Range = range;
+          }
+          public static implicit operator int[](PositionInfo range)
+          {
+               return new int[] { range.Left, range.Right };
+          }
+          public static implicit operator PositionInfo(int[] range)
+          {
+               if (range.Length != 2)
+                    throw new Exception("Conversion failed");
+               return new PositionInfo(String.Format("{0}-{1}", range[0], range[1]));
+          }
+          public static bool operator ==(PositionInfo range1, PositionInfo range2)
+          {
+               return range1.Left == range2.Left && range1.Right == range2.Right ? true : false;
+          }
+          public static bool operator !=(PositionInfo range1, PositionInfo range2)
+          {
+               return range1.Left != range2.Left && range1.Right != range2.Right ? true : false;
+          }
+
+          public override bool Equals(object obj)
+          {
+               return base.Equals(obj);
+          }
+          public override int GetHashCode()
+          {
+               return base.GetHashCode();
+          }
+          public override string ToString()
+          {
+               return base.ToString();
+          }
+     }
+}
---
+++ AlgoLabs/Start.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AlgoLabs
+{
+     public class Start
+     {
+          static void Main(string[] args)
+          {                                                                                         
+               TimeSpan runningTime = new TimeSpan();
+               Console.WriteLine("Welcome to Algorithms' Lab Solutions");
+               int[] array = { 3, 1, 7, 5, 4, 2, 8, 6 }, bubbleSortResult, heapSortResult, insertionSortResult, mergeSortResult, quickSortResult;
+               Console.WriteLine("\n{0}\t{1}\t\t{2}\t\t\t{3}", "Algorithm", "Style", "Values", "Running Time");
+               array.Print<int, SortAlgorithms>(SortAlgorithms.None, new Nullable<TimeSpan>());
+
+               //array.Sort(out bubbleSortResult, out runningTime, algorithm: SortAlgorithms.Bubble);
+               //bubbleSortResult.Print(SortAlgorithms.Bubble, MethodCallStyle.Recursive, runningTime);
+               array.Sort(out bubbleSortResult, out runningTime, algorithm: SortAlgorithms.Bubble, callStyle: MethodCallStyle.Iterative);
+               bubbleSortResult.Print(SortAlgorithms.Bubble, MethodCallStyle.Iterative, runningTime);
+
+               //array.Sort(out heapSortResult, out runningTime, algorithm: SortAlgorithms.Heap);
+               //heapSortResult.Print(SortAlgorithms.Heap, MethodCallStyle.Recursive, runningTime);
+               //array.Sort(out heapSortResult, out runningTime, algorithm: SortAlgorithms.Heap, callStyle: MethodCallStyle.Iterative);
+               //heapSortResult.Print(SortAlgorithms.Heap, MethodCallStyle.Iterative, runningTime);
+
+               //array.Sort(out insertionSortResult, out runningTime, algorithm: SortAlgorithms.Insertion);
+               //insertionSortResult.Print(SortAlgorithms.Insertion, MethodCallStyle.Recursive, runningTime);
+               //array.Sort(out insertionSortResult, out runningTime, algorithm: SortAlgorithms.Insertion, callStyle: MethodCallStyle.Iterative);
+               //insertionSortResult.Print(SortAlgorithms.Insertion, MethodCallStyle.Iterative, runningTime);
+
+               array.Sort(out mergeSortResult, out runningTime, algorithm: SortAlgorithms.Merge);
+               Console.WriteLine();
+               mergeSortResult.Print(SortAlgorithms.Merge, MethodCallStyle.Recursive, runningTime);
+               array.Sort(out mergeSortResult, out runningTime, algorithm: SortAlgorithms.Merge, callStyle: MethodCallStyle.Iterative);
+               mergeSortResult.Print(SortAlgorithms.Merge, MethodCallStyle.Iterative, runningTime);
+
+               array.Sort(out quickSortResult, out runningTime, algorithm: SortAlgorithms.Quick);
+               quickSortResult.Print(SortAlgorithms.Quick, MethodCallStyle.Recursive, runningTime);
+               array.Sort(out quickSortResult, out runningTime, algorithm: SortAlgorithms.Quick, callStyle: MethodCallStyle.Iterative);
+               quickSortResult.Print(SortAlgorithms.Quick, MethodCallStyle.Iterative, runningTime);
+               Console.ReadKey();
+               
+          }
+     }
+}
